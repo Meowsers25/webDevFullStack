4d8eed8e5c422b46f8dfb1d4e959b1e0af631565
Christian D. Lamothe <c.lamothe25@gmail.com>
2018-02-13 18:15:14 +0000

    Using splat * if a route is undefined. Route params

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -17,6 +17,19 @@ app.get("/dog", function(req, res) {
     console.log("Someone made a request to /dog ");
     res.send("MEOW!");
 });
+//route params
+app.get("/r/:subredditName", function(req, res){
+    var sub = req.params.subredditName;
+    res.send("Welcome to the " + sub.toUpperCase() + " subreddit"); 
+});
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+   res.send("COMMENT SECTION"); 
+});
+//use the splat(*) catch all if a route is undefined
+//route order is key! The splat needs to come at the end
+app.get("*", function(req, res) {
+   res.send("You are now in the Matrix"); 
+});
 
 //Tell Express to listen for requests(start server)
 
