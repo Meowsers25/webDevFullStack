2fa17bfcecf193758d62f7bb97cce3cf21ef2238
Christian D. Lamothe <c.lamothe25@gmail.com>
2018-02-15 15:19:38 +0000

    Custom assets; Linked to external stylesheet in 'public' dir. Cleaned up code by using app.set. Included partials

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,13 +1,19 @@
 var express = require("express");
 var app = express();
 
+//this tells express to look in the 'public' folder
+app.use(express.static("public"));
+
+//this line tells express that it will expect .ejs files to be rendered
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-   res.render("home.ejs");
+   res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res) {
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
     //res.send("You fell in love with " + thing); 
 });
 
@@ -17,7 +23,7 @@ app.get("/posts", function(req, res) {
        {title: "Dogs Suck", author: "Sully"},
        {title: "Holy Crap I'm Scared", author: "Beatrice"}
     ]; 
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
