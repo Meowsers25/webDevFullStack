0694c130606f8631d6927b3fe22b7e8e8e582495
Christian D. Lamothe <c.lamothe25@gmail.com>
2018-02-21 01:50:50 +0000

    Added show.ejs.

--- YelpCamp/ver2/app.js
+++ YelpCamp/ver2/app.js
@@ -14,7 +14,8 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 //Creates schema so we can use the different methods available
@@ -23,7 +24,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //Create campground in DB
 // Campground.create({
 //     name: "Tall Peak", 
-//     image: "https://ridb.recreation.gov/images/83164.jpg"
+//     image: "https://ridb.recreation.gov/images/83164.jpg",
+//     description: "This is a tall peak. No bathrooms/water. Beautiful views."
 // }, function(err, campground) {
 //     if(err) {
 //         console.log(err);
@@ -33,26 +35,30 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //     }
 // });
 
-app.get("/", function(req, res) {
-   res.render("landing"); 
-});
+// app.get("/", function(req, res) {
+//   res.render("landing"); 
+// });
 
+
+//INDEX - DISPLAYS LIST
 app.get("/campgrounds", function(req, res) {
     //Get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds) {
         if(err) {
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     });
    //res.render("campgrounds", {campgrounds: campgrounds});
 });
 
+//CREATE - ADDS NEW DOG TO DB
 app.post("/campgrounds", function(req, res) {
    var name = req.body.name;
-   var image = req.body.img;
-   var newCampground = {name: name, image: image};
+   var image = req.body.image;
+   var desc = req.body.description;
+   var newCampground = {name: name, image: image, description: desc};
    //campgrounds.push(newCampground); we arent usig this anymore
    //We now want to create a new campground and save to DB
    Campground.create(newCampground, function(err, newlyCreated) {
@@ -64,8 +70,25 @@ app.post("/campgrounds", function(req, res) {
    });
 });
 
+
+//NEW - SHOW FORM TO CREATE NEW CAMPGROUND
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
+});
+
+//route order matters here!
+//SHOW - Shows more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+    //find the campground with the provided ID
+    //render show template with that campground
+   //res.send("THIS WILL BE THE SHOWPAGE SOME DAY"); 
+   Campground.findById(req.params.id, function(err, foundCampground) {
+      if(err) {
+          console.log(err);
+      } else {
+          res.render("show", {campground: foundCampground});
+      }
+   });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
--- YelpCamp/ver2/readme.md
+++ YelpCamp/ver2/readme.md
@@ -2,3 +2,9 @@
 * Install and configure Mongoose
 * Setup campground model
 * Use campground model inside of our routes
+ 
+#Show page
+* Review the RESTful routes we've seen so far
+* Add a description to our campground model
+* Show db.collection.drop()
+* Add a show route/template
