7a421d4cc45f14cc2440cfd75ebfad6552b77adb
Christian D. Lamothe <c.lamothe25@gmail.com>
2018-03-05 13:29:50 +0000

    adding styling

--- RESTful_routing/RESTful_blog_app/app.js
+++ RESTful_routing/RESTful_blog_app/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
+var expressSanitizer = require('express-sanitizer');
 var bodyParser = require('body-parser');
 var app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 //MONGOOSE/MODEL CONFIG
@@ -46,6 +48,7 @@ app.get("/blogs/new", function(req, res) {
 //CREATE ROUTE
 app.post("/blogs", function(req, res) {
    //create blog
+   req.body.blog.body = req.sanitize(req.body.blog.body);
    Blog.create(req.body.blog, function(err, newBlog) {
        if(err) {
            res.render("new");
@@ -80,6 +83,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 //UPDATE ROUTE
 app.put("/blogs/:id", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
         if(err) {
             res.redirect("/blogs");
